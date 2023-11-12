fd095e80cfd4090d55d16d3e608a559a0246722e
Eart <thepray1313@gmail.com>
2023-11-12 13:22:02 +0700

    fix:connect real api

--- api/index.js
+++ api/index.js
@@ -18,7 +18,8 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://eart-blog-frontend.vercel.app/"],
+    // origin: "http://localhost:5173",
+    origin: "https://eart-blog-frontend.vercel.app/",
     credentials: true, // allow cookie
   })
 );
